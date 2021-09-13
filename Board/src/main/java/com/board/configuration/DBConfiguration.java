c8d7f0eaee314a2da136e7f73e69f08d443ff86c
Kimbonggyu <kbgkbg1999@gmail.com>
2021-09-13 17:54:34 +0900

    - develop Interceptor & AOP and Transaction

--- Board/src/main/java/com/board/configuration/DBConfiguration.java
+++ Board/src/main/java/com/board/configuration/DBConfiguration.java
@@ -11,12 +11,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @PropertySource("classpath:/application.properties")
+@EnableTransactionManagement
 public class DBConfiguration {
 
 	@Autowired
@@ -53,6 +57,11 @@ public class DBConfiguration {
 	@ConfigurationProperties(prefix = "mybatis.configuration")
 	public org.apache.ibatis.session.Configuration mybatisConfig() {
 		return new org.apache.ibatis.session.Configuration();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 
 }

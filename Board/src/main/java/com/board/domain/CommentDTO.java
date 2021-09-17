3791e9843caf1e62fa54c94bd48f6789d3038bf7
Kimbonggyu <kbgkbg1999@gmail.com>
2021-09-17 14:21:42 +0900

    - develop comment test code - will make comment function restful

---
+++ Board/src/main/java/com/board/domain/CommentDTO.java
@@ -0,0 +1,16 @@
+package com.board.domain;
+
+import lombok.Data;
+
+@Data
+public class CommentDTO extends CommonDTO{
+	
+	private Long idx;
+	
+	private Long boardIdx;
+	
+	private String content;
+	
+	private String writer;
+
+}
